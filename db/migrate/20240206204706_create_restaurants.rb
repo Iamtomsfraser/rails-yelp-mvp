1dabedd5aba5d17bfb46844ff0c4d920d1aff3fd
thomas fraser <thomas@thefrasers.co>
2024-02-10 00:29:56 +0000

    completed

--- db/migrate/20240206204706_create_restaurants.rb
+++ db/migrate/20240206204706_create_restaurants.rb
@@ -2,13 +2,10 @@ class CreateRestaurants < ActiveRecord::Migration[7.1]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.string :string
       t.string :address
-      t.string :string
       t.string :phone_number
       t.string :integer
       t.string :category
-      t.string :string
 
       t.timestamps
     end
